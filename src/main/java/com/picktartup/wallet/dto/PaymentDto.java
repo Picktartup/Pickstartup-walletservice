4ed23379d7459fe72aabeb79ea4ab25af4e8d42d
yyyeun <yummytomato@naver.com>
2024-12-02 15:47:39 +0900

    🐛 fix: PaymentDto, UserServiceClient 수정

--- src/main/java/com/picktartup/wallet/dto/PaymentDto.java
+++ src/main/java/com/picktartup/wallet/dto/PaymentDto.java
@@ -20,14 +20,14 @@ public class PaymentDto {
         @Builder
         public static class Request {
             @NotBlank(message = "주문번호는 필수입니다")
-            private Long transactionId;
+            private String transactionId;
 
             @NotNull(message = "사용자 ID는 필수입니다")
             private Long userId;
 
             @NotNull(message = "결제 금액은 필수입니다")
             @Positive(message = "결제 금액은 0보다 커야 합니다")
-            private BigDecimal amount;
+            private BigDecimal cashAmount;
 
             private String paymentMethod;
         }
@@ -49,7 +49,7 @@ public class PaymentDto {
     @AllArgsConstructor
     @Builder
     public static class CompletedEvent {
-        private Long transactionId;
+        private String transactionId;
         private Long userId;
         private BigDecimal amount;
 
@@ -61,7 +61,7 @@ public class PaymentDto {
             return CompletedEvent.builder()
                     .transactionId(request.getTransactionId())
                     .userId(request.getUserId())
-                    .amount(request.getAmount())
+                    .amount(request.getCashAmount())
                     .build();
         }
 
